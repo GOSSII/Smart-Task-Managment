e01c6a312653665a7957a57e1a53a70dc2742d9e
Dipeshpuri Goswami <dipsgos7@gmail.com>
2022-12-07 02:27:03 -0500

    Added OnBoardingScreen

---
+++ lib/onBoading/onboarding.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+import 'package:smart_task_management/first.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  final Color kDarkBlueColor = Color(0xFF053149);
+
+  OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OnBoardingSlider(
+      finishButtonText: 'Register',
+      onFinish: () {
+        Navigator.push(
+          context,
+          CupertinoPageRoute(
+            builder: (context) => FirstPage(),
+          ),
+        );
+      },
+      finishButtonColor: kDarkBlueColor,
+      skipTextButton: Text(
+        'Skip',
+        style: TextStyle(
+          fontSize: 16,
+          color: kDarkBlueColor,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      trailing: Text(
+        'Login',
+        style: TextStyle(
+          fontSize: 16,
+          color: kDarkBlueColor,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      trailingFunction: () {
+        Navigator.push(
+          context,
+          CupertinoPageRoute(
+            builder: (context) => FirstPage(),
+          ),
+        );
+      },
+      controllerColor: kDarkBlueColor,
+      totalPage: 3,
+      headerBackgroundColor: Colors.white,
+      pageBackgroundColor: Colors.white,
+      background: [
+        Image.asset(
+          'assets/4942219.jpg',
+          height: 400,
+        ),
+        Image.asset(
+          'assets/2810266.jpg',
+          height: 400,
+        ),
+        Image.asset(
+          'assets/20943642.jpg',
+          height: 400,
+        ),
+      ],
+      speed: 1.8,
+      pageBodies: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 480,
+              ),
+              Text(
+                'Manage Your Task',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: kDarkBlueColor,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'With This Small App You Can Organize \n All Your Tasks and Duties In A One Single App',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 480,
+              ),
+              Text(
+                'Plan Your Day',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: kDarkBlueColor,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Add A Task And The App Will Remind You.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 480,
+              ),
+              Text(
+                'Accomplish Your Goals',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: kDarkBlueColor,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Track Your Activities And Accomplish Your Goal.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
